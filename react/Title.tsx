14b31e96c983377ca4e8fb8dcfd7cc4c4a464b45
Jennifer Farias Rodrigues <jennifer.farias.rodrigues@gmail.com>
2022-02-03 19:34:10 -0300

    arquivo title

--- react/Title.tsx
+++ react/Title.tsx
@@ -7,7 +7,7 @@ const CSS_HANDLES = ['title']
 
 const Title: StorefrontFunctionComponent<TitleProps> = ({ title }) => {
   const handles = useCssHandles(CSS_HANDLES)
-  const titleText = title || <FormattedMessage id="countdown.title" />
+  const titleText = title || <FormattedMessage id="cashback.title" />
 
   return (
     <div className={`${handles.title} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
@@ -21,12 +21,12 @@ interface TitleProps {
 }
 
 Title.schema = {
-  title: 'editor.countdown-title.title',
-  description: 'editor.countdown-title.description',
+  title: 'editor.cashback-title.title',
+  description: 'editor.cashback-title.description',
   type: 'object',
   properties: {
     title: {
-      title: 'I am a title',
+      title: 'I am a title cashback',
       type: 'string',
       default: null,
     },
